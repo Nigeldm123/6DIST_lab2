14e3651b3c342afecf5f64bef1696b80be9596eb
Nigel <nigel.dm@telenet.be>
2024-03-16 13:32:34 +0100

    Deleted all empty files.

--- src/main/data/data.sql
+++
@@ -1,4 +0,0 @@
-insert into bankaccount(name) values('Nigel');
-insert into bankaccount(name) values('Femke');
-insert into bankaccount(name) values('Gones');
-insert into bankaccount(name) values('Rien');
--- src/main/java/com/distributedSystems/Banking/BankController.java
+++
@@ -1,9 +0,0 @@
-package com.distributedSystems.Banking;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class BankController {
-
-}
